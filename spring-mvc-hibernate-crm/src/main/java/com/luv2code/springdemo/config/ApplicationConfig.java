71d5a808b84dad4b534526d4d92ab6ec34340cc6
Greg Parsons <gregorskii@gmail.com>
2017-07-14 11:36:01 -0700

    adds hibernate crm demo app
    fixes some things on base

---
+++ spring-mvc-hibernate-crm/src/main/java/com/luv2code/springdemo/config/ApplicationConfig.java
@@ -0,0 +1,20 @@
+package com.luv2code.springdemo.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
+
+import javax.persistence.EntityManagerFactory;
+
+@Configuration
+@EnableAutoConfiguration
+public class ApplicationConfig {
+
+    @Bean
+    public HibernateJpaSessionFactoryBean sessionFactory(EntityManagerFactory emf) {
+        HibernateJpaSessionFactoryBean fact = new HibernateJpaSessionFactoryBean();
+        fact.setEntityManagerFactory(emf);
+        return fact;
+    }
+}
