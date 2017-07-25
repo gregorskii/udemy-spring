6105a2b804e9c0466cf2c2895d8a558245b5166c
Greg Parsons <gregorskii@gmail.com>
2017-07-25 15:48:47 -0700

    adds aop
    adds css/js to base

---
+++ spring-mvc-aop/src/main/java/com/luv2code/springdemo/config/ApplicationConfig.java
@@ -0,0 +1,6 @@
+package com.luv2code.springdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig { }
---
+++ spring-mvc-aop/src/main/java/com/luv2code/springdemo/controllers/LandingController.java
@@ -0,0 +1,13 @@
+package com.luv2code.springdemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LandingController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index/index";
+    }
+}
